23e236bb0222812f072b6ba3a15215456374aee7
prashanthrajendran <prashanth09587@gmail.com>
2012-11-25 22:08:56 +0530

    Update crawler/LinkExtracter.py

--- crawler/LinkExtracter.py
+++ crawler/LinkExtracter.py
@@ -1,5 +1,5 @@
 import sys
-
+import urllib
 from lxml.html import parse
 from urllib.request import urlopen
 from URLValidator import URLValidator
@@ -19,7 +19,6 @@ class LinkExtracter:
             response = urlopen(url)
             contenttype = response.info()['Content-Type']
             contenttype = contenttype.strip().lower()
-            
             '''
             if the conten-type is not text/html the url is rejected as it could be images or something else
             that could not be parsed 
@@ -28,9 +27,13 @@ class LinkExtracter:
                 self.log.debug('rejected = '+url+' Content-Type = '+contenttype)
                 return
             dom = parse(response).getroot()
-        except Exception as  e:
+        except urllib.error.HTTPError as e1:
+            self.log.warn(''.join(['HTTP error for ',url]))
+            self.log.debug(e1,exc_info=sys.exc_info()[2]) 
+            return
+        except Exception as  e2:
             self.log.warn('cannot parse '+url)
-            self.log.debug(e,exc_info=sys.exc_info()[2]) 
+            self.log.debug(e2,exc_info=sys.exc_info()[2]) 
             return
         if not dom is None:
             links = dom.cssselect('a')
@@ -39,8 +42,13 @@ class LinkExtracter:
                 for link in links:
                     if not link is None: 
                         href = link.get('href')
-                        if URLValidator.isValidURL(href):
-                            linksList.append(href)
+                        if href is None:
+                            continue
+                        suburl = URLValidator.formValidURL(url,href)
+                        if not suburl is None:
+                            linksList.append(suburl)
+                        else:
+                            self.log.debug('rejected href = '+href)
                 if not linksList:
                     return None
                 else:
