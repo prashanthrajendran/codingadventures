f5447464d59548bc44795b4e3cefeb5a9b024434
prashanthrajendran <prashanth09587@gmail.com>
2012-11-25 22:08:03 +0530

    Update crawler/URLValidator.py

--- crawler/URLValidator.py
+++ crawler/URLValidator.py
@@ -1,13 +1,27 @@
-from urllib.parse import urlparse
+from urllib.parse import urljoin
 
 class URLValidator:
     
     @staticmethod
     def isValidURL(link):
         link = str(link)
-        parseResult = urlparse(link)
-        protocols = ['http','https']
-        if not parseResult.scheme in protocols:
+        if link.startswith('http') or link.startswith('https'):
+            return True
+        else:
+            return False 
+        
+    @staticmethod
+    def isValidSubUrl(link):
+        if link.startswith('#'):
             return False
         else:
             return True
+    
+    @staticmethod
+    def formValidURL(link,sublink):
+        if not URLValidator.isValidSubUrl(sublink):
+            return None
+        else:
+            return urljoin(link,sublink)    
+            
+        
